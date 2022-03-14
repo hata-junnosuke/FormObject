ad88e644d0f3f802a4c30e309079797efa9611eb
hata-junnosuke <jh19911209@icloud.com>
2022-03-14 20:11:05 +0900

    2つのモデルに登録可能

--- app/controllers/formats_controller.rb
+++ app/controllers/formats_controller.rb
@@ -1,19 +1,24 @@
 class FormatsController < ApplicationController
   def index
-    @formats = User.all
+    @formats = Company.includes(:user).all
   end
 
   def new
-    @format = User.new
+    @format = CompanyUser.new
   end
 
   def create
-    @format = User.create(format_params)
-    redirect_to root_path
+    @format = CompanyUser.new(format_params)
+    if @format.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
+
   def format_params
-    params.require(:user).permit(:name, :name_kana, :nickname)
+    params.require(:company_user).permit(company_attributes: %i[name address], user_attributes: %i[user_name email password password_confirmation])
   end
 end
--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,4 @@
 class Company < ApplicationRecord
+  has_one :user
+
 end
